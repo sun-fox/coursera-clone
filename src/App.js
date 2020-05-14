a6109c7535e5284d05821bcf2b54b3c009443590
sun-fox <1999sunnykumarsingh@gmail.com>
2020-05-14 11:45:37 +0530

    Added components except navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import './css/bootstrap.min.css';
+import Header from './components/Header';
+import Collaborator from './components/Collaborator';
+import Features from './components/Features';
+import Section1 from './components/Section1';
+import Section2 from './components/Section2';
+import Section3 from './components/Section3';
+import Section4 from './components/Section4';
+import Section5 from './components/Section5';
+import Section6 from './components/Section6';
+import Section7 from './components/Section7';
+import Section8 from './components/Section8';
+import Footer from './components/Footer';
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <Header/>
+        <Collaborator/>
+        <Features/>
+        <Section1/>
+        <Section2/>
+        <Section3/>
+        <Section4/>
+        <Section5/>
+        <Section6/>
+        <Section7/>
+        <Section8/>
+        <Footer/>
     </div>
   );
 }
